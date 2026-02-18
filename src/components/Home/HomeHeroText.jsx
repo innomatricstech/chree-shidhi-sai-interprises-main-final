f2a5e9ae2f797c872c6cd1eae11feabdb9a7901b
Selvam Thirumal <selvamthirumal22@gmail.com>
2026-02-18 18:26:27 +0530

    Updated gitignore

--- src/components/Home/HomeHeroText.jsx
+++ src/components/Home/HomeHeroText.jsx
@@ -1,29 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const HomeHeroText = () => {
   return (
-    <div className="container absolute -my-[50%] lg:-my-[0%] md:-my-[0%] md:labsolute md:top-0 lg:absolute lg:right-96 lg:top-[300px]">
+    <div className="container absolute md:absolute lg:absolute lg:right-96 lg:top-[300px]">
+      
       <div className="text-left px-4 md:mx-[45%] w-[300px]">
-      {/* <p className="text-sm md:text-base my-2 text-white lg:text-lg">
-        Connecting the Nation
-        </p> */}
-        <h1 className="text-xl text-white font-semibold mb-2 md:text-5xl lg:text-6xl">
-  CHETHAN <span className="animate-colorChange">CONSTRUCTIONS</span>
-</h1>
+
+        {/* Company Name */}
+        <h1 className="text-2xl text-white font-bold mb-2 md:text-6xl lg:text-7xl leading-tight">
+          Shree Chethana
+          <span className="block animate-colorChange">Services</span>
+        </h1>
+
+        {/* Tagline */}
         <p className="text-sm md:text-base my-4 text-white lg:text-lg">
-        Redefining <span className='text-red-500 font-bold'>Architecture </span>
+          Redefining{" "}
+          <span className="text-red-500 font-bold">Architecture</span>
         </p>
+
+        {/* Contact Button */}
         <Link to={"/contact"}>
-        <button className="w-44 h-12 max-w-xs px-4 py-2  bg-gray-50  text-zinc-950 
-        hover:text-white font-bold 
-         shadow-md hover:bg-amber-400 focus:outline-none 
-         focus:ring-2 focus:ring-amber-300 focus:ring-opacity-50 transition duration-300 ease-in-out">
-  
-  Contact Us
-  
-  </button>
-  </Link>
+          <button
+            className="w-44 h-12 max-w-xs px-4 py-2 bg-gray-50 text-zinc-950
+            hover:text-white font-bold shadow-md hover:bg-amber-400
+            focus:outline-none focus:ring-2 focus:ring-amber-300
+            focus:ring-opacity-50 transition duration-300 ease-in-out"
+          >
+            Contact Us
+          </button>
+        </Link>
+
       </div>
     </div>
   );
